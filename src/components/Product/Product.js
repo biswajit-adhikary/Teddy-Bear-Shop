77a4e92327bca28ffdc7158f415cb5cf712f6ad3
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-03-26 01:48:55 +0600

    Font Awesome Added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Product = (props) => {
     console.log(props);
@@ -13,7 +15,7 @@ const Product = (props) => {
                 <div className="product-text">
                     <h3>{name}</h3>
                     <h4>{price}</h4>
-                    <button type="button" className="btn btn-primary"><span>Add to List</span></button>
+                    <button type="button" className="btn btn-primary"><span>Add to List</span> <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></button>
                 </div>
             </div>
         </div>
