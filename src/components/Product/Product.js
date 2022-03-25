743cd965d3fa9f6d576c2d6272cca589d177e5b0
biswajit-adhikary <biswajitadhikary.web@gmail.com>
2022-03-26 01:44:10 +0600

    Display Product Done

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './Product.css';
 
-const Product = () => {
+const Product = (props) => {
+    console.log(props);
+    const { name, price, picture } = props.bear;
     return (
-        <div>
-            <h2>This is product</h2>
+        <div className='col-lg-4'>
+            <div className="single-product">
+                <div className="product-image">
+                    <img src={picture} alt="" />
+                </div>
+                <div className="product-text">
+                    <h3>{name}</h3>
+                    <h4>{price}</h4>
+                    <button type="button" className="btn btn-primary"><span>Add to List</span></button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
+    const [bears, setBears] = useState([]);
+    useEffect(() => {
+        fetch('teddy-bear.json')
+            .then(rec => rec.json())
+            .then(data => setBears(data));
+    }, [])
     return (
         <div className='shop-area'>
             <div className="container">
                 <div className="row">
                     <div className="col-lg-9">
                         <div className="products">
-                            {
-                                <Product></Product>
-                            }
+                            <div className="row">
+                                {
+                                    bears.map(bear => <Product
+                                        key={bear.id}
+                                        bear={bear}
+                                    ></Product>)
+                                }
+                            </div>
                         </div>
                     </div>
                     <div className="col-lg-3">
@@ -24,7 +35,7 @@ const Shop = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
